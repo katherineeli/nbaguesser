fdfeed6d63daf42bead6ed59910b1e0a58ce0146
Katherine Li <likatherinee@gmail.com>
2020-10-06 22:42:00 -0400

    Configure basic NavBar visuals

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
 import RenderPlayer from "./components/renderPlayer.js";
+import NavBar from "./components/NavBar.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <NavBar/>
                 
         <form className="guess-player">
                     
---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import '../styles/styles.css';
+
+class NavBar extends Component {
+    render () {
+        return (
+            <div id="navBar" className="navBar">
+                <div id="title" className="h1 navTitle">NBA GUESSER</div> 
+                <nav>
+                    <a id="leaderboard" className="navButton h2" style={{left: 1144, color:"black", textDecoration:"none"}}>LEADERBOARD</a>
+                    <a id="login" className="navButton h2" style={{left: 1326, color:"black", textDecoration:"none"}}>LOGIN</a>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
+export default NavBar;
