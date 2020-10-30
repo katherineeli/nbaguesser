97562c6d7079d78a0d577624de8af0875a5edfb1
Katherine Li <likatherinee@gmail.com>
2020-10-29 21:43:22 -0400

    replace content when timer runs out

--- src/App.js
+++ src/App.js
@@ -1,27 +1,18 @@
 import React, { Component } from "react";
 import RenderPlayer from "./components/renderPlayer.js";
 import NavBar from "./components/NavBar.js";
-import Timer from "./components/timer.js";
+import '../node_modules/bulma';
+import './styles/styles.css';
+
 
 
 class App extends Component {
-  state = {
-    timeout: false
-  }
-
-  setTimeout(){
-    this.setState({
-      timeout: true
-    })
-  }
 
   render() {
     return (
       <div className="App">
-        <NavBar/>       
-        {/* <Timer timeoutCallback = {this.setTimeout.bind(this)}/> */}
-        <RenderPlayer timeout = {this.state.timeout}/>
-              
+        <NavBar/>
+        <RenderPlayer />
       </div>
     );
   }
